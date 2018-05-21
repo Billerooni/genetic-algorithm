b9f538fec46f47c71fdd0d0affaf286c432a4159
Billerooni <33398995+Billerooni@users.noreply.github.com>
2018-05-21 21:46:52 +0000

    Update main.cpp
    Added:
    Each generation is looped through properly
    The parents that will breed are chosen, weighted to those who are fitter
    The creature with the highest fitness now has its parent displayed

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
 /*	A simple genetic algorithm to solve linear equations in the form
-	ax + b = c.
-*/
+	ax + b = c. */
 
 #include <cstdlib>
 #include <cstdio>
 #include <ctime>
 #include <cmath>
 #include <vector>
+#include <string>
 
-// Constants
+// Amount of equations for the creatures to be tested on
 const int TESTS = 1;
+// Amount of nodes the creatures uses for calculation
 const int NODE_AMOUNT = 3;
+// Amount of creatures in the population
 const int CREATURES = 100;
+// Amount of generations for the simulation to run for
 const int GENERATIONS = 100;
+// Chance of a gene mutating when it is passed on
 const double MUTATION_RATE = 0.05;
 
 // Creature
@@ -27,7 +31,12 @@ class Creature {
 	// Outputs x
 	double output = 0;
 
+	/*	Parent 1 Index, Parent 1 Generation
+		Parent 2 Index, Parent 2 Generation */
+	int parents[4] = { -1, -1, -1, -1 };
+
 public:
+	// Initialise the creature with a seed (used for first generation only)
 	void seed () {
 		for (unsigned i = 0; i < sizeof(node_weights)/sizeof(double); i++)
 			node_weights[i] = 2*rand()/(RAND_MAX + 1.0);
@@ -40,6 +49,20 @@ public:
 		input[0] = a;
 		input[1] = b;
 		input[2] = c;
+	}
+
+	// Parents
+	void set_parents (int p[4]) {
+		for (unsigned i = 0; i<4; i++)
+			parents[i] = p[i];
+	}
+	std::string get_parents () {
+		if (parents[0] < 0)
+			return "N/A";
+		return std::to_string(parents[0]) + "G" +
+			std::to_string(parents[1]) + "," +
+			std::to_string(parents[2]) + "G" +
+			std::to_string(parents[3]);
 	}
 
 	// Get the output from the genes and input
@@ -81,6 +104,8 @@ public:
 
 	// Breed with another creature
 	Creature breed (Creature c) {
+		// TODO
+
 		return Creature();
 	}
 };
@@ -105,46 +130,87 @@ int main () {
 	}
 	printf("\n");
 
+	// Initialise the population
+	std::vector<Creature> population = {};
+	population.resize(CREATURES);
+
+	std::vector<double> fitnesses = {};
+	fitnesses.resize(CREATURES);
+
 	// Loop through each generation
 	for (unsigned gen = 0; gen < GENERATIONS; gen++) {
-		// Initialise the population
-		std::vector<Creature> population = {};
-		population.resize(CREATURES);
-
-		std::vector<double> fitnesses = {};
-		fitnesses.resize(CREATURES);
-
 		// Loop through each creature
-		for (unsigned j = 0; j < population.size(); j++) {
-			// TODO: Seeds new creatures
-			population[j].seed();
+		for (unsigned cr = 0; cr < CREATURES; cr++) {
+			// Seed the first population
+			if (gen == 0)
+				population[cr].seed();
 
 			// Set the fitness to the mean fitness of each test
-			fitnesses[j] = population[j].get_avg_fitness(tests);
+			fitnesses[cr] = population[cr].get_avg_fitness(tests);
 		}
 
 		// Get the highest and mean fitness of the generation
 		double sumFitness = 0;
-		double highestFitness = 0;
+		double hFit = 0;
 
-		for (unsigned j = 0; j < fitnesses.size(); j++) {
+		for (unsigned cr = 0; cr < CREATURES; cr++) {
 			// Get the current highest fitness
-			if (fitnesses[j] > highestFitness)
-				highestFitness = fitnesses[j];
+			if (fitnesses[cr] > fitnesses[hFit])
+				hFit = cr;
 
 			// Add to the fitness sum, used to work out the mean
-			sumFitness += fitnesses[j];
+			sumFitness += fitnesses[cr];
 		}
 
 		// Print information
 		if (!(gen % 10) || gen == GENERATIONS-1) {
 			printf("GENERATION %u\n", gen);
 			printf("Mean fitness: %f\n", sumFitness/fitnesses.size());
-			printf("Highest fitness: %f\n\n", highestFitness);
+			printf("Highest fitness: %f (Parents: %s)\n\n",
+				fitnesses[hFit], population[hFit].get_parents().c_str());
 		}
 
-		// TODO: Work out which creatures will be parents for the next generation
+		// Breed and create the next generation
+		for (unsigned j = 0; j < CREATURES; j++) {
+			/*	Loop through each creature to see which pair will
+				breed the current new creature. Fitter creatures have
+				a higher chance of breeding. */
 
+			// First parent
+			unsigned p1;
+			double sumFit = 0;
+			double pSeed = rand()/(RAND_MAX + 1.0);
+
+			for (unsigned cr = 0; cr < CREATURES; cr++) {
+				/*	Gets a random creature to breed with, weighted by
+					its fitness. */
+				if (pSeed * sumFitness - sumFit < fitnesses[cr]) {
+					p1 = cr;
+					break;
+				}
+				sumFit += fitnesses[cr];
+			}
+
+			// Second parent
+			unsigned p2;
+			do {
+				sumFit = 0;
+				pSeed = rand()/(RAND_MAX + 1.0);
+
+				for (unsigned cr = 0; cr < CREATURES; cr++) {
+					/*	Gets a random creature to breed with, weighted
+						by its fitness. This cannot be the same as the
+						first parent. */
+					if (pSeed * sumFitness - sumFit < fitnesses[cr]) {
+						p2 = cr;
+						break;
+					}
+					sumFit += fitnesses[cr];
+				}
+			} while	(p1 == p2);
+
+			// TODO: Breeding
+		}
 	}
 
 	return 0;
